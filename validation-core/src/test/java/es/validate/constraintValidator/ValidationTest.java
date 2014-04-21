63850a6ab0217e3f05270a6c953a53b555921147
José María González Monghit <jm.gmonghit@gmail.com>
2014-04-21 20:12:48 +0200

    validation

--- validation-core/src/test/java/es/validate/constraintValidator/ValidationTest.java
+++ validation-core/src/test/java/es/validate/constraintValidator/ValidationTest.java
@@ -2,18 +2,34 @@ package es.validate.constraintValidator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import es.validate.constraint.IsNotSQLInyection;
+
+
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:validation-core-unit-test.xml")
-public class ValidationTest {
 
+
+
+public class ValidationTest {
+	
+	@Autowired
+	private IsNotSQLInyection isNotSQLInyection;
+	
+    @Ignore
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isNotSQLInyectionTest() {
+		
+		Assert.assertTrue(true);
+		
 	}
 
 }
