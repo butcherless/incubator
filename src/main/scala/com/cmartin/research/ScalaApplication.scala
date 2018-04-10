57a5b4aaebcb8cd54f1d1efbe2923a6bb0351287
butcherless <cmarting@gmail.com>
2018-04-11 00:26:54 +0200

    application configuration; code cleanup

--- src/main/scala/com/cmartin/research/ScalaApplication.scala
+++ src/main/scala/com/cmartin/research/ScalaApplication.scala
@@ -1,10 +1,6 @@
 package com.cmartin.research
 
-import java.time.LocalDate
-
-import com.cmartin.domain.{AircraftBean, Person}
-import com.cmartin.repository.{AircraftRepository, PersonRepository}
-import com.cmartin.research.DataManager.getLocalDate
+import com.cmartin.repository.PersonRepository
 import org.neo4j.ogm.session.SessionFactory
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,28 +8,26 @@ import org.springframework.boot.{ApplicationRunner, SpringApplication}
 import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager
-import org.springframework.transaction.annotation.{EnableTransactionManagement, Transactional}
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
-//@Configuration
-//@ComponentScan
-//@EnableAutoConfiguration
 @EnableNeo4jRepositories(Array("com.cmartin.repository"))
 @SpringBootApplication
 class ScalaApplication {
   val log: Logger = LoggerFactory.getLogger(classOf[ScalaApplication])
 
-  class DependencyAnalyzer {
-    def hello = s"scala class ${this.toString}"
+
+
+
+  @Configuration
+  class AppConfig(repo: PersonRepository){
+
+    @Bean def dependencyAnalyzer() = new DependencyAnalyzer(repo)
   }
-
-  //@NodeEntity
-  //class Person(@BeanProperty @Id @GeneratedValue id: java.lang.Long, @BeanProperty name: String)
-
 
   @Configuration
   @EnableTransactionManagement
   class MyConfiguration {
-    @Bean def dependencyAnalyzer() = new DependencyAnalyzer()
+
 
     @Bean def configuration() = {
       new org.neo4j.ogm.config.Configuration.Builder()
@@ -52,27 +46,36 @@ class ScalaApplication {
     def transactionManager(): Neo4jTransactionManager = {
       new Neo4jTransactionManager(sessionFactory())
     }
+
   }
 
 
-  @Bean
-  @Transactional
-  //def init(da: DependencyAnalyzer): ApplicationRunner = args => {
-  def init(personRepo: PersonRepository, aircraftRepo: AircraftRepository): ApplicationRunner = args => {
-    log.debug("ScalaApplication/SpringBoot initialization")
-    log.debug(s"person entity count: ${personRepo.count}")
-    //DataManager.loadData
-    val p = new Person("pepe")
-    personRepo.save(p)
-    log.debug(s"person entity count: ${personRepo.count}")
+  //@Bean
+  def init(da: DependencyAnalyzer): ApplicationRunner = args => {
+    log.debug(s"dependency analyzer: ${da.analyze}")
 
-
-    val ac = AircraftBean(regNo = "ec-lxr", engineNo = 2, name = "Pico de las Nieves", deliverDate = getLocalDate(2015, 9, 1))
-    aircraftRepo.save(ac)
-    log.debug(s"aircraft entity count: ${aircraftRepo.count}")
-
-    log.debug("all data has been deleted")
+    log.debug(s"person count: ${da.personCount}")
   }
+
+  /*
+    //@Bean
+    //def init(da: DependencyAnalyzer): ApplicationRunner = args => {
+    def init(personRepo: PersonRepository, aircraftRepo: AircraftRepository): ApplicationRunner = args => {
+      log.debug("ScalaApplication/SpringBoot initialization")
+      log.debug(s"person entity count: ${personRepo.count}")
+      //DataManager.loadData
+      val p = new Person("pepe")
+      personRepo.save(p)
+      log.debug(s"person entity count: ${personRepo.count}")
+
+
+      val ac = AircraftBean(regNo = "ec-lxr", engineNo = 2, name = "Pico de las Nieves", deliverDate = getLocalDate(2015, 9, 1))
+      aircraftRepo.save(ac)
+      log.debug(s"aircraft entity count: ${aircraftRepo.count}")
+
+      log.debug("all data has been deleted")
+    }
+  */
 }
 
 // application runner
@@ -86,8 +89,16 @@ object ScalaApplication extends App {
 
   val context = SpringApplication.run(classOf[ScalaApplication], args: _*)
 
+  val da = context.getBean(classOf[DependencyAnalyzer])
+  log.debug(s"dependency analyzer: ${da.analyze}")
+  log.debug(s"person count: ${da.personCount}")
   // Only for debugging, remove when needed
   //debugContextBeans()
 
 }
 
+class DependencyAnalyzer(repo: PersonRepository) {
+  def analyze = s"analyzing dependencies"
+
+  def personCount = repo.count()
+}
