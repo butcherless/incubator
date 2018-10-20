3244e8b5597ab9e509c1b99931c6b470517fc86e
Carlos Martin <cmarting@gmail.com>
2018-10-20 10:16:00 +0200

    repository tests refactor, trait fixture pattern

--- repository/src/test/scala/com/cmartin/learn/RepositorySpec.scala
+++ repository/src/test/scala/com/cmartin/learn/RepositorySpec.scala
@@ -16,74 +16,69 @@ class RepositorySpec extends FlatSpec with Matchers {
   val uuidRegex = "[a-z0-9-]{36}"
 
   // CREATE
-  it should "save an aircraft into the repository" in {
-    val repo = fixture.repository
-    val beforeCount = repo.count()
+  it should "save an aircraft into the repository" in new Repository {
+    val beforeCount = repository.count()
     val a = newAircraft(typeCodeBoeing, registrationMIG)
 
     // functionality
-    val aSaved = repo.save(a)
+    val aSaved = repository.save(a)
 
     beforeCount.value shouldBe 0
-    repo.count().value shouldBe 1
+    repository.count().value shouldBe 1
     aSaved.value.nonEmpty shouldBe true
     aSaved.value.matches(uuidRegex) shouldBe true
   }
 
 
   // READ
-  it should "retrieve an aircraft from the repository" in {
+  it should "retrieve an aircraft from the repository" in new Repository {
     // preconditions
-    val repo = fixture.repository
     val a = newAircraft(typeCodeBoeing, registrationMIG)
-    val aSaved = repo.save(a)
+    val aSaved = repository.save(a)
 
     // functionality
-    val result: Option[Aircraft] = repo.findById(aSaved.value)
+    val result: Option[Aircraft] = repository.findById(aSaved.value)
 
     // verifications
     result.value.typeCode shouldEqual a.typeCode
     result.value.registration shouldEqual a.registration
   }
 
-  it should "return None type for not found id" in {
+  it should "return None type for not found id" in new Repository {
     // preconditions
-    val repo = fixture.repository
 
     // functionality
-    val result: Option[Aircraft] = repo.findById("dummy-id")
+    val result: Option[Aircraft] = repository.findById("dummy-id")
 
     // verifications
     result shouldBe None
   }
 
-  it should "retrieve only airbus aircraft from the repository" in {
+  it should "retrieve only airbus aircraft from the repository" in new Repository {
     // preconditions
-    val repo = fixture.repository
-    val aSaved = repo.save(newAircraft(typeCodeBoeing, registrationMIG))
-    val bSaved = repo.save(newAircraft(typeCodeAirbus, registrationMXV))
+    val aSaved = repository.save(newAircraft(typeCodeBoeing, registrationMIG))
+    val bSaved = repository.save(newAircraft(typeCodeAirbus, registrationMXV))
 
     // functionality
-    val list = repo.findAll(_.typeCode == typeCodeAirbus)
+    val list = repository.findAll(_.typeCode == typeCodeAirbus)
 
     // verifications
-    repo.count().value shouldEqual 2
+    repository.count().value shouldEqual 2
     list.size shouldEqual 1
     list.value.head.typeCode shouldEqual typeCodeAirbus
   }
 
 
   // UPDATE
-  it should "update an aircraft into the repository" in {
+  it should "update an aircraft into the repository" in new Repository {
     // preconditions
-    val repo = fixture.repository
     val a = newAircraft(typeCodeBoeing, registrationMIG)
-    val aSaved = repo.save(a)
-    val aRetrieved: Option[Aircraft] = repo.findById(aSaved.value)
+    val aSaved = repository.save(a)
+    val aRetrieved: Option[Aircraft] = repository.findById(aSaved.value)
 
     // functionality
-    repo.save(aRetrieved.value.copy(typeCode = typeCodeAirbus, registration = registrationMXV))
-    val aUpdated = repo.findById(aSaved.value)
+    repository.save(aRetrieved.value.copy(typeCode = typeCodeAirbus, registration = registrationMXV))
+    val aUpdated = repository.findById(aSaved.value)
 
     // verifications
     aUpdated.value.typeCode shouldEqual typeCodeAirbus
@@ -92,34 +87,32 @@ class RepositorySpec extends FlatSpec with Matchers {
 
 
   // REMOVE
-  it should "remove an aircraft from the repository" in {
+  it should "remove an aircraft from the repository" in new Repository {
     // preconditions
-    val repo = fixture.repository
     val created = newAircraft(typeCodeBoeing, registrationMIG)
-    val aSaved = repo.save(created)
-    val beforeCount = repo.count()
+    val aSaved = repository.save(created)
+    val beforeCount = repository.count()
 
     // functionality
-    val retrieved = repo.findById(aSaved.value)
-    val res: Option[String] = repo.remove(retrieved.value)
+    val retrieved = repository.findById(aSaved.value)
+    val res: Option[String] = repository.remove(retrieved.value)
 
     // verifications
-    val afterCount = repo.count()
+    val afterCount = repository.count()
     res.value shouldEqual aSaved.value
     beforeCount.value - afterCount.value shouldEqual 1
   }
 
-  it should "remove only airbus aircraft from the repository" in {
+  it should "remove only airbus aircraft from the repository" in new Repository {
     // preconditions
-    val repo = fixture.repository
-    repo.save(newAircraft(typeCodeBoeing, registrationMIG))
-    val aSaved = repo.save(newAircraft(typeCodeAirbus, registrationMXV))
+    repository.save(newAircraft(typeCodeBoeing, registrationMIG))
+    val aSaved = repository.save(newAircraft(typeCodeAirbus, registrationMXV))
 
     // functionality
-    val list = repo.removeAll(_.typeCode == typeCodeAirbus)
+    val list = repository.removeAll(_.typeCode == typeCodeAirbus)
 
     // verifications
-    repo.count().value shouldEqual 1
+    repository.count().value shouldEqual 1
     list.size shouldEqual 1
     list.value.head shouldEqual aSaved.value
   }
@@ -135,8 +128,11 @@ class RepositorySpec extends FlatSpec with Matchers {
   */
 
 
-  def fixture = new {
-    lazy val repository = MemoryRepository()
+  /*
+  Test fixture
+   */
+  trait Repository {
+    val repository = MemoryRepository()
   }
 
   private def newAircraft(typeCode: String, registration: String) = Aircraft(typeCode = typeCode, registration = registration)
