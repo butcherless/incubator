c92a9138669d9478ba95c1e44b48f92f049277d4
Carlos Martin <cmarting@gmail.com>
2018-10-20 20:58:55 +0200

    added insert and retrieve operations for an entity

--- repository/src/main/scala/com/cmartin/learn/repository/package.scala
+++ repository/src/main/scala/com/cmartin/learn/repository/package.scala
@@ -1,15 +1,22 @@
 package com.cmartin.learn.repository
 
 import slick.jdbc.H2Profile.api._
-import slick.lifted.ProvenShape
 
 
 package object frm {
 
-  case class Aircraft(id: Long, typeCode: String, registration: String)
+  object TableNames {
+    val fleet = "FLEET"
+  }
 
+  object TypeCodes {
+    val AIRBUS_350_900 = "A359"
+    val BOEING_787_800 = "B788"
+  }
 
-  class Fleet(tag: Tag) extends Table[(Long, String, String)](tag, "AIRCRAFT") {
+  case class Aircraft(id: Option[Long], typeCode: String, registration: String)
+
+  class Fleet(tag: Tag) extends Table[Aircraft](tag, TableNames.fleet) {
     // This is the primary key column:
     def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
 
@@ -17,26 +24,9 @@ package object frm {
 
     def registration = column[String]("REGISTRATION")
 
-    // Every table needs a * projection with the same type as the table's type parameter
-    def * : ProvenShape[(Long, String, String)] = (id, typeCode, registration)
+    def * = (id.?, typeCode, registration) <> (Aircraft.tupled, Aircraft.unapply)
   }
 
-  /*
-    class Fleet(tag: Tag) extends Table[Aircraft](tag, "AIRCRAFT") {
-      // This is the primary key column:
-      def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
-
-      def typeCode = column[String]("TYPE_CODE")
-
-      def registration = column[String]("REGISTRATION")
-
-      // Every table needs a * projection with the same type as the table's type parameter
-      def * : ProvenShape[Aircraft] = (id, typeCode, registration) <> (Aircraft.tupled, Aircraft.unapply)
-
-      //TODO
-      //def * = id ~ typeCode ~ registration <> (Aircraft.apply _, Aircraft.unapply _)
-    }
-  */
   lazy val fleet = TableQuery[Fleet]
 
 }
--- repository/src/test/scala/com/cmartin/learn/SlickSpec.scala
+++ repository/src/test/scala/com/cmartin/learn/SlickSpec.scala
@@ -1,6 +1,6 @@
 package com.cmartin.learn
 
-import com.cmartin.learn.repository.frm.Fleet
+import com.cmartin.learn.repository.frm.{Aircraft, Fleet, TableNames, TypeCodes}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
@@ -10,21 +10,57 @@ import slick.jdbc.meta.MTable
 class SlickSpec extends FlatSpec with Matchers with BeforeAndAfter with ScalaFutures {
   implicit override val patienceConfig = PatienceConfig(timeout = Span(5, Seconds))
 
+  val registrationMIG = "ec-mig"
+
   val fleet = TableQuery[Fleet]
 
   var db: Database = _
+
+
+  it should "create the fleet database" in {
+    val tables = db.run(MTable.getTables).futureValue
+
+    tables.size shouldBe 1
+    tables.count(_.name.name == TableNames.fleet) shouldBe 1
+  }
+
+  it should "insert an aircraft" in {
+    val count = insertAircraft()
+
+    count shouldBe 1
+  }
+
+  it should "retrieve an aircraft" in {
+    insertAircraft()
+
+    val list = db.run(fleet.filter(_.registration === registrationMIG).result).futureValue
+
+    list.size shouldBe 1
+    list.head.registration shouldBe registrationMIG
+  }
+
+
+  /*
+   _    _   ______   _        _____    ______   _____     _____
+  | |  | | |  ____| | |      |  __ \  |  ____| |  __ \   / ____|
+  | |__| | | |__    | |      | |__) | | |__    | |__) | | (___
+  |  __  | |  __|   | |      |  ___/  |  __|   |  _  /   \___ \
+  | |  | | | |____  | |____  | |      | |____  | | \ \   ____) |
+  |_|  |_| |______| |______| |_|      |______| |_|  \_\ |_____/
+  */
+
+  def insertAircraft(): Int =
+    db.run(fleet += Aircraft(None, TypeCodes.BOEING_787_800, registrationMIG)).futureValue
 
   def createSchema() = db.run((fleet.schema).create).futureValue
 
   before {
     db = Database.forConfig("h2mem")
-  }
-
-  it should "create the fleet database" in {
     createSchema()
-    
-    val tables = db.run(MTable.getTables).futureValue
-
-    tables.size shouldBe 1
   }
+
+  after {
+    db.close
+  }
+
 }
