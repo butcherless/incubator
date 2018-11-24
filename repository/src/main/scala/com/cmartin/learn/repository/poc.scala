4ba0987cf20aa84beeb4d01a01bd4d3b5b7f8306
Carlos Martin <cmarting@gmail.com>
2018-11-24 21:40:57 +0100

    removed generic repository poc

--- repository/src/main/scala/com/cmartin/learn/repository/poc.scala
+++ repository/src/main/scala/com/cmartin/learn/repository/poc.scala
@@ -1,56 +1,6 @@
 package com.cmartin.learn
 
-import slick.jdbc.H2Profile.api._
-
 package object poc {
 
-  trait BaseEntity[K] {
-    val id: K
-  }
-
-  abstract class LongBaseEntity extends BaseEntity[Option[Long]]
-
-  final case class User(name: String, id: Option[Long] = None) extends LongBaseEntity
-
-  final case class Message(text: String, userId: Long = 0, id: Option[Long] = None) extends LongBaseEntity
-
-  abstract class BaseTable[E <: LongBaseEntity](tag: Tag, tableName: String) extends Table[E](tag, tableName) {
-    // primary key column:
-    def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
-
-  }
-
-  final class UserTable(tag: Tag) extends BaseTable[User](tag, "USERS") {
-    // property columns:
-    def name = column[String]("NAME")
-
-    def * = (name, id.?) <> (User.tupled, User.unapply)
-  }
-
-  final class MessageTable(tag: Tag) extends BaseTable[Message](tag, "MESSAGES") {
-    // property columns:
-    def name = column[String]("NAME")
-
-    // foreign columns:
-    def userId = column[Long]("USER_ID")
-
-    def * = (name, userId, id.?) <> (Message.tupled, Message.unapply)
-  }
-
-  abstract class BaseRepository[E <: LongBaseEntity, T <: BaseTable[E]](db: Database) {
-    protected val entities: TableQuery[T]
-
-    def findById(id: Long) = entities.filter(_.id === id).result
-
-    def count() = db.run(entities.length.result)
-  }
-
-  class UserRepository(implicit db: Database) extends BaseRepository[User, UserTable](db) {
-    protected lazy val entities = TableQuery[UserTable]
-  }
-
-  class MessageRepository(implicit db: Database) extends BaseRepository[Message, MessageTable](db) {
-    protected lazy val entities = TableQuery[MessageTable]
-  }
-
+  def echo(x: Int) = x
 }
--- repository/src/test/scala/com/cmartin/learn/PocSpec.scala
+++ repository/src/test/scala/com/cmartin/learn/PocSpec.scala
@@ -2,117 +2,14 @@ package com.cmartin.learn
 
 import com.cmartin.learn.poc._
 import org.scalatest._
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{Seconds, Span}
-import slick.jdbc.H2Profile
-import slick.jdbc.H2Profile.api._
-import slick.jdbc.meta.MTable
 
 
-class PocSpec extends FlatSpec with Matchers with OptionValues with ScalaFutures with BeforeAndAfterEach {
-  implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(5, Seconds))
+class PocSpec extends FlatSpec with Matchers {
 
-  val userName = "cmartin"
-  val userId = Some(7L)
-  val text = "message text"
-
-  implicit var db : Database = _
-
-  /*
-  it should "test user entity with default id" in {
-    val user = User(userName)
-
-    user.name shouldBe userName
-    user.id shouldBe None
+  it should "return the same value" in {
+    val inputValue = 1
+    val result = echo(inputValue)
+    result shouldBe inputValue
   }
-
-  it should "test user entity" in {
-    val user = User(userName, userId)
-
-    user.name shouldBe userName
-    user.id shouldBe userId
-  }
-
-  it should "test message entity" in {
-    val message = Message(text, userId.getOrElse(0L))
-
-    message.text shouldBe text
-    message.userId shouldBe userId.value
-  }
-*/
-
-
-  it should "return zero user count" in new UserRepo {
-    val count = userRepository.count()
-
-    val result = count.futureValue
-
-    result shouldBe 0
-  }
-
-  it should "return zero message count" in new UserRepo {
-    val count = messageRepository.count()
-
-    val result = count.futureValue
-
-    result shouldBe 0
-  }
-
-  class UserRepo {
-    val userRepository : UserRepository = new UserRepository
-    val messageRepository : MessageRepository = new MessageRepository
-  }
-
-  override def beforeEach(): Unit = {
-    db = Database.forConfig("h2mem")
-
-    val schemaActionList = List(TableQuery[UserTable], TableQuery[MessageTable])
-
-    db.run(DBIO.sequence(schemaActionList.map(_.schema.create)))
-
-  }
-
-  override def afterEach() = {
-    db.close()
-  }
-  //    implicit var db: Database = _
-
-
-  //  def createSchema() = {
-  //    val schemaAction = (
-  //      userRepository.entities.schema
-  //      ).create
-  //
-  //    db.run(schemaAction).futureValue
-  //  }
-
-
-  //    db = Database.forConfig("h2mem")
-  //    val userRepository = new UserRepository
-  //    val schemaAction = (
-  //      userRepository.entities.schema
-  //      ).create
-  //
-  //    db.run(schemaAction).futureValue
-  //
-
 
 }
-
-/*
-trait DB extends BeforeAndAfterAll { this:  Suite =>
-  implicit val db = Database.forConfig("h2mem")
-  val userRepository = new UserRepository
-
-  override def beforeAll() = {
-    def createSchema() = {
-          val schemaAction = (
-            userRepository.entities.schema
-            ).create
-
-          db.run(schemaAction)
-        }
-  }
-  override def afterAll() = db.close()
-}
-*/
