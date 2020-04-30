17e59a3d82f3fc788d0f26b2f24680aa60b909fa
Scala Steward <me@scala-steward.org>
2020-04-30 16:17:22 +0200

    Update akka-actor-typed, akka-stream to 2.6.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val akkaHttpVersion     = "10.1.11"
-  lazy val akkaVersion         = "2.6.4"
+  lazy val akkaVersion         = "2.6.5"
   lazy val catsVersion         = "2.0.0"
   lazy val configVersion       = "1.4.0"
   lazy val h2Version           = "1.4.200"
