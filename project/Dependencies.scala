bb5096479318e404c6eca7e54136bf5d543ba3c9
Scala Steward <me@scala-steward.org>
2020-08-26 22:09:03 +0200

    Update tapir-akka-http-server, tapir-core, ... to 0.16.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.30"
   lazy val slickVersion        = "3.3.2"
   lazy val utestVersion        = "0.6.6"
-  lazy val tapirVersion        = "0.16.15"
+  lazy val tapirVersion        = "0.16.16"
   lazy val zioVersion          = "1.0.1"
 
   // production code
