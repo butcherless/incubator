c313e26240e2fbc482356daf398fde94fdb7f329
Scala Steward <me@scala-steward.org>
2021-03-23 16:07:22 +0100

    Update scala-logging to 3.9.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val logback      = "1.2.3"
   val playJson     = "2.9.2"
   val quill        = "3.7.0"
-  val scalaLogging = "3.9.2"
+  val scalaLogging = "3.9.3"
   val scalaz       = "7.2.27"
   val scalatest    = "3.2.6"
   val slf4j        = "1.7.30"
