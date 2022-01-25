c3038600f6df4e22004c38a52a967011ae7dddfa
Scala Steward <me@scala-steward.org>
2022-01-25 14:24:16 +0100

    Update slf4j-api to 1.7.35

--- project/Versions.scala
+++ project/Versions.scala
@@ -12,7 +12,7 @@ object Versions {
   val scalaLogging = "3.9.3"
   val scalaz       = "7.2.27"
   val scalatest    = "3.2.11"
-  val slf4j        = "1.7.34"
+  val slf4j        = "1.7.35"
   val slick        = "3.3.3"
   val utest        = "0.6.6"
   // val zio          = "1.0.8"
