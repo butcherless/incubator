9f24ffbab0847208ed56e62e56ad478d402f02fb
Carlos Martin <cmarting@gmail.com>
2022-03-19 11:17:11 +0100

    * upgrade zio lib * fix breaking changes

--- neo4j-repository/src/main/scala/com/cmartin/learn/Touchdown.scala
+++ neo4j-repository/src/main/scala/com/cmartin/learn/Touchdown.scala
@@ -1,18 +1,12 @@
 package com.cmartin.learn
 
 import org.neo4j.driver._
-import org.neo4j.driver.async.AsyncSession
-import org.neo4j.driver.async.ResultCursor
+import org.neo4j.driver.async.{AsyncSession, ResultCursor}
 import zio.Runtime.{default => runtime}
 import zio._
 
-import java.security.Provider.Service
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CompletionStage
-import scala.concurrent.Future
-
 object Touchdown {
-  // Create a managed driver
+  // Create a scoped driver
 
   /** Country object
     * @param name
@@ -30,10 +24,11 @@ object Touchdown {
   val pass      = "my-pass"
   val authToken = AuthTokens.basic(user, pass)
 
-  def acquire(uri: String, authTokens: AuthToken): Driver =
-    GraphDatabase.driver(uri, authTokens)
-  def release(driver: Driver): Unit                       =
-    driver.close()
+  def acquire(uri: String)(authTokens: AuthToken): Task[Driver] =
+    Task.attempt(GraphDatabase.driver(uri, authTokens))
+
+  def release(driver: Driver): UIO[Unit] =
+    Task.succeed(driver.close())
 
   val readPersonByNameQuery: String =
     """MATCH (p:Person)
@@ -41,8 +36,8 @@ object Touchdown {
         RETURN p.name AS name"""
   val params                        = Map("person_name" -> "personName")
 
-  val managedDriver: TaskManaged[Driver] =
-    ZManaged.acquireReleaseAttemptWith(acquire(uri, authToken))(release)
+  val scopedDriver: RIO[Scope, Driver] =
+    ZIO.acquireRelease(acquire(uri)(authToken))(release)
 
   val s1: Session      = ???
   val r1               = s1.run(readPersonByNameQuery)
@@ -53,38 +48,42 @@ object Touchdown {
   val data1 = rec1.get("name").asString()
 
   val driver1: Driver          = ???
-  val fut1                     = driver1.asyncSession().runAsync(readPersonByNameQuery).toCompletableFuture()
+  val fut1                     = driver1.asyncSession().runAsync(readPersonByNameQuery).toCompletableFuture
   val zio1: Task[ResultCursor] = ZIO.fromCompletableFuture(fut1)
 
-  val zio2 = managedDriver.use { driver =>
-    ZIO.fromCompletableFuture(
-      driver.asyncSession()
-        .runAsync(readPersonByNameQuery)
-        .toCompletableFuture()
-    )
+  val zio2 = ZIO.scoped {
+    scopedDriver.flatMap { driver =>
+      ZIO.fromCompletableFuture(
+        driver.asyncSession()
+          .runAsync(readPersonByNameQuery)
+          .toCompletableFuture
+      )
+    }
   }
 
   val query: RIO[Driver, ResultCursor] = for {
     driver  <- ZIO.service[Driver]
     session <- UIO.succeed(driver.session())
-    results <- managedDriver.use { driver =>
+    results <- ZIO.scoped(scopedDriver.flatMap { driver =>
                  ZIO.fromCompletableFuture(
                    driver.asyncSession()
                      .runAsync(readPersonByNameQuery)
-                     .toCompletableFuture()
+                     .toCompletableFuture
                  )
-               }
+               })
   } yield results
 
-  def acquireSession(driver: Driver): Session =
-    driver.session()
-  def closeSession(session: Session): Unit    =
-    session.close()
+  def acquireSession(driver: Driver): Task[Session] =
+    Task.attempt(driver.session())
+  def closeSession(session: Session): UIO[Unit]     =
+    Task.succeed(session.close())
 
-  val managedSession: TaskManaged[Session] =
-    ZManaged.acquireReleaseAttemptWith(acquireSession(driver1))(closeSession)
+  val scopedSession =
+    ZIO.acquireRelease(acquireSession(driver1))(closeSession)
 
-  val l1 = ZLayer.fromManaged(managedDriver)
+  val l1 = ZLayer.fromAcquireRelease(acquireSession(driver1))(closeSession)
+  val l3 = ZLayer.scoped(scopedSession)
+  val l2 = ZLayer.fromZIO(scopedSession)
 
   sealed trait DatabaseError
   case class FieldMappingError(message: String)    extends DatabaseError
@@ -95,17 +94,18 @@ object Touchdown {
   }
 
   case class Neo4jCountryRepository(driver: Driver) extends CountryRepository {
-    val managedSession: Managed[DatabaseError, Session] =
-      ZManaged.acquireReleaseAttemptWith(acquireSession(driver))(closeSession)
-        .mapError(e => DefaultDatabaseError(e.getMessage()))
+    val scopedSession: ZIO[Scope, DefaultDatabaseError, Session] =
+      ZIO.acquireRelease(acquireSession(driver1))(closeSession)
+        .mapError(e => DefaultDatabaseError(e.getMessage))
 
-    override def findByCode(code: String): IO[DatabaseError, Country] = {
-      managedSession.use { session =>
-        Task.attempt(session.run(readPersonByNameQuery).single())
-          .flatMap(extractCountry)
-          .mapError(e => manageError(e, s"findByCode($code)"))
+    override def findByCode(code: String): IO[DatabaseError, Country] =
+      ZIO.scoped {
+        scopedSession.flatMap { session =>
+          Task.attempt(session.run(readPersonByNameQuery).single())
+            .flatMap(extractCountry)
+            .mapError(e => manageError(e, s"findByCode($code)"))
+        }
       }
-    }
 
     /* Country helpers
         - Country extractor
@@ -136,7 +136,7 @@ object Touchdown {
   object Main {
 
     val driverLayer: TaskLayer[Driver] =
-      ZLayer.fromManaged(managedDriver)
+      ZLayer.scoped(scopedDriver)
 
     val dbEnv: TaskLayer[CountryRepository with Driver] =
       ZLayer.make[CountryRepository with Driver](
