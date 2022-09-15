bcd7b6ce9b9581c67a0d55d4a4cbd482f189e02f
Scala Steward <scala_steward@virtuslab.com>
2022-09-15 13:11:51 +0000

    Update slf4j-api to 2.0.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val quill         = "3.10.0"
   val quillPostgres = "3.10.0"
   val scalatest     = "3.3.0-SNAP2"
-  val slf4j         = "2.0.0"
+  val slf4j         = "2.0.1"
   val slick         = "3.4.1"
   val zio           = "2.0.2"
   val zioLogging    = "2.1.1"
