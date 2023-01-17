96c4ad99541f010004b12821f6f22cc035f08cac
Carlos Martin <cmarting@gmail.com>
2023-01-17 12:53:44 +0100

    * updgrade zio lib

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val scalatest     = "3.2.15"
   val slf4j         = "2.0.6"
   val slick         = "3.4.1"
-  val zio           = "2.0.5"
+  val zio           = "2.0.6"
   val zioLogging    = "2.1.7"
   val zioPrelude    = "1.0.0-RC16"
 
