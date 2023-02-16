dea459d34c822af7d74d8e4819ab181f29fc47b2
Scala Steward <scala_steward@virtuslab.com>
2023-02-16 14:37:02 +0000

    Update zio, zio-test, zio-test-magnolia, ... to 2.0.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val scalatest     = "3.2.15"
   val slf4j         = "2.0.6"
   val slick         = "3.4.1"
-  val zio           = "2.0.8"
+  val zio           = "2.0.9"
   val zioLogging    = "2.1.9"
   val zioPrelude    = "1.0.0-RC16"
 
