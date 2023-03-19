db83fdc2393f58fc9b0390438867cccdd30c1f06
Carlos Martin <cmarting@gmail.com>
2023-03-20 00:44:52 +0100

    * add implementation

---
+++ bluezone/src/main/scala/com/cmartin/bluezone/AppConfigurator.scala
@@ -0,0 +1,38 @@
+package com.cmartin.bluezone
+
+import com.cmartin.bluezone.Model.{ DomainError, PayRequest, Rate, Ticket }
+import zio.{ IO, ZIO }
+
+final case class AppConfigurator(
+    rateProvider: ForObtainingRates,
+    ticketStore: ForStoringTickets,
+    paymentService: ForPaying
+) extends ForConfiguringApp {
+
+  override def createRates(rates: List[Rate]): IO[DomainError, Unit] =
+    for {
+      _ <- rateProvider.empty()
+      _ <- ZIO.foreachDiscard(rates)(rate =>
+             ZIO.whenZIO(rateProvider.exists(rate.name))(rateProvider.addRate(rate))
+           )
+    } yield ()
+
+  override def createTicket(ticket: Ticket): IO[DomainError, Unit] =
+    ZIO
+      .whenZIO(ticketStore.exists(ticket.code))(ticketStore.store(ticket))
+      .as(())
+
+  override def eraseTicket(ticketCode: String): IO[DomainError, Unit] =
+    ZIO
+      .whenZIO(ticketStore.exists(ticketCode))(ticketStore.delete(ticketCode))
+      .as(())
+
+  override def setNextTicketCodeToReturn(ticketCode: String): IO[DomainError, Unit] =
+    ticketStore.setNextCode(ticketCode)
+
+  override def getNextTicketCodeToReturn(): IO[DomainError, String] =
+    ticketStore.nextAvailableCode()
+
+  override def getLastPayRequestDone(): IO[DomainError, PayRequest] =
+    paymentService.lastPayRequest()
+}
