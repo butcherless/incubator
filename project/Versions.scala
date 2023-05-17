97d0f4345f55579cbd7d4873032679b2afcf6c75
Carlos Martin <cmarting@gmail.com>
2023-05-17 19:52:00 +0200

    * upgrade scalatest

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val postgres      = "42.6.0"
   val quill         = "3.10.0"
   val quillPostgres = "3.10.0"
-  val scalatest     = "3.2.15"
+  val scalatest     = "3.2.16"
   val slf4j         = "2.0.7"
   val slick         = "3.4.1"
   val zio           = "2.0.13"
