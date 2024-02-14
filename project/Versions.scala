962094d2ff94425713975dc6bfdd63e5f2779682
Carlos Martin <cmarting@gmail.com>
2024-02-14 19:58:57 +0100

    Update Versions.scala

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   val zio           = "2.0.20"
   val zioConfig     = "4.0.1"
   val zioLogging    = "2.2.0"
-  val zioPrelude    = "1.0.0-RC22"
+  val zioPrelude    = "1.0.0-RC23"
 
   val scalaVersion = "2.13.12"
 }
