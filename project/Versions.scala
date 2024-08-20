0c4bd517ab7166d590963bafbe52547ef4386c64
Carlos Martin <cmarting@gmail.com>
2024-08-20 16:41:05 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   val zio           = "2.1.7"
   val zioConfig     = "4.0.2"
   val zioLogging    = "2.3.0"
-  val zioPrelude    = "1.0.0-RC28"
+  val zioPrelude    = "1.0.0-RC29"
 
   val scala = "2.13.14"
 }
