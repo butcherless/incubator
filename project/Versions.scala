31ec5c7fa8574d4d8b4038a5bf3959b4ca7f14cc
Scala Steward <scala_steward@virtuslab.com>
2024-11-11 14:35:49 +0000

    Update zio-prelude to 1.0.0-RC32

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   val zio           = "2.1.12"
   val zioConfig     = "4.0.2"
   val zioLogging    = "2.3.2"
-  val zioPrelude    = "1.0.0-RC31"
+  val zioPrelude    = "1.0.0-RC32"
 
   val scala = "2.13.15"
 }
