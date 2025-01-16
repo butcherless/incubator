70fb83946b1a2b88944123d75719b5df3bbc0c8b
Scala Steward <scala_steward@virtuslab.com>
2025-01-16 14:49:51 +0000

    Update postgresql to 42.7.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val h2            = "2.3.232"
   val neo4j         = "5.27.0"
   val playJson      = "2.10.6"
-  val postgres      = "42.7.4"
+  val postgres      = "42.7.5"
   val quill         = "3.10.0"
   val quillPostgres = "3.10.0"
   val scalatest     = "3.2.19"
